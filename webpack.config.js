8e33e2ad0a4f5ea14894008a5d2060d1d9f364fc
Jason Hoi <jason@de3interactive.com>
2022-05-26 16:26:30 +0800

    add html plugin, css extract and minify

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,49 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+require('dotenv').config();
+
 module.exports = {
-  mode: 'development',
-  devtool: false,
+  mode: process.env.APP_MODE || 'production',
+  context: '/private/var/www/webpack-demo',
   entry: './src/index.js',
   output: {
-    filename: 'app.[contenthash].js',
+    filename: 'assets/js/main.bundle.[contenthash].js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Webpack 5 App',
+      template: './src/template.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'assets/css/[name].[contenthash].css',
+    }),
+    new CssMinimizerPlugin(),
+  ],
+  optimization: {
+    emitOnErrors: false,
+    minimize: true,
+    // there is a JS minizer blocking issue when using CssMinimizerPlugin here
+    // minimizer: [new CssMinimizerPlugin()],
   },
   module: {
     rules: [
       {
-        test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              esModule: false,
+            },
+          },
+          'css-loader',
+          'sass-loader',
+        ],
       },
     ],
   },
