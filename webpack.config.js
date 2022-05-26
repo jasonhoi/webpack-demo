63457cb185389a325f63c1a81a91c1df0fb7cfcf
Jason Hoi <jason@de3interactive.com>
2022-05-26 13:01:12 +0800

    use bootstrap 4 webpack, and main.scss

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,18 @@
 const path = require('path');
-
 module.exports = {
   mode: 'development',
-  devtool: 'inline-source-map',
+  devtool: false,
   entry: './src/index.js',
   output: {
     filename: 'app.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
   },
 };
