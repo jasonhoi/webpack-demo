fa4141d247561f71cd479fcd1dd0e10c2d9527ac
Jason Hoi <jason@de3interactive.com>
2022-05-27 10:46:14 +0800

    setup webpack-dev-server

--- webpack.common.js
+++ webpack.common.js
@@ -11,6 +11,15 @@ module.exports = {
     clean: true,
     path: path.resolve(__dirname, 'dist'),
   },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 9000,
+    hot: false,
+    liveReload: true,
+  },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Webpack app',
